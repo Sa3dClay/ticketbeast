dc826ce009223e70413965e87542f4673d6ace09
abdulrahman atef <abdulrahman.atef@iken.tech>
2022-04-08 01:52:33 +0200

    add fake payment gateway exception

--- tests/Unit/Billing/FakePaymentGatewayTest.php
+++ tests/Unit/Billing/FakePaymentGatewayTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Billing;
 
 use Tests\CreatesApplication;
 use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentFailedException;
 use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 class FakePaymentGatewayTest extends BaseTestCase
@@ -20,5 +21,19 @@ class FakePaymentGatewayTest extends BaseTestCase
     $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
 
     $this->assertEquals(2500, $paymentGateway->totalCharges());
+  }
+
+  /**
+   * @test
+   */
+  public function charges_with_an_invalid_payment_token_fail()
+  {
+    try {
+      $paymentGateway = new FakePaymentGateway;
+      $paymentGateway->charge(2500, 'invalid-payment-token');
+    } catch (PaymentFailedException $e) {
+      return;
+    }
+    $this->fail();
   }
 }
